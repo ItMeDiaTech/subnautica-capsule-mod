ea9cd56e158daa2f8be11574b42603f93449805e
Austin Jordan <austingjordan@gmail..com>
2026-02-13 20:18:00 -0500

    Bump version to 1.2.0 for check-for-updates feature

--- src/SubnauticaCapsule.Installer/InstallerVersion.cs
+++ src/SubnauticaCapsule.Installer/InstallerVersion.cs
@@ -3,5 +3,5 @@ namespace SubnauticaCapsule.Installer;
 internal static class InstallerVersion
 {
     // Keep in sync with PluginInfo.Version in SubnauticaCapsule.Mod
-    public const string Current = "1.1.0";
+    public const string Current = "1.2.0";
 }
--- src/SubnauticaCapsule.Mod/PluginInfo.cs
+++ src/SubnauticaCapsule.Mod/PluginInfo.cs
@@ -2,7 +2,8 @@ namespace SubnauticaCapsule;
 
 internal static class PluginInfo
 {
+    // Also referenced as a config filename in SubnauticaCapsule.Installer/ModInstaller.cs
     public const string GUID = "com.diatech.unlimitedtimecapsules";
     public const string Name = "Unlimited Time Capsules";
-    public const string Version = "1.1.0";
+    public const string Version = "1.2.0";
 }
